992c83a008ed9524e7a47aaa6a6ee42a150b94a4
Victor Zemtsov <viczem.dev@gmail.com>
2022-05-26 09:03:17 +0300

    Add pykeepass==4.0.1
    - Replace socket data format from json to plain text
    - Fix fetching entities contains slashes in path
    - Move code into one file (socket opening by the plugin)

--- README.md
+++ README.md
@@ -1,69 +1,51 @@
 # Ansible KeePass Lookup Plugin
 
-Perhaps, from a security view point, this solution is the same as `ansible-vault`.
-Just if you are storing secrets data in KeePass, then why not use it, 
-instead of duplicating to `ansible-vault`. 
+The plugin allows to read data from KeePass file (modifying is not supported)
+
+## How it works
+
+The plugin opens a UNIX socket with decrypted KeePass file. 
+For performance reasons, decryption occurs only once at socket startup, 
+and the KeePass file remains decrypted as long as the socket is open.
+The UNIX socket file is stored in a temporary folder according to OS.
 
 
 ## Installation
 
-Dependency: `pykeepass==3.2.1`
+Requirements: `python 3`, `pykeepass==4.0.1`
 
-    pip install 'pykeepass==3.2.1' --user
+    pip install 'pykeepass==4.0.1' --user
     mkdir -p ~/.ansible/plugins/lookup && cd "$_"
-    curl https://raw.githubusercontent.com/viczem/ansible-keepass/master/keepass.py -o ./keepass.py
-
-[More about ansible plugins installation](https://docs.ansible.com/ansible/latest/dev_guide/developing_locally.html)
+    curl https://raw.githubusercontent.com/viczem/ansible-keepass/main/keepass.py -o ./keepass.py
 
 
 ## Variables
 
 - `keepass_dbx` - path to KeePass file
-- `keepass_psw` - password. [*optional*] if the socket is used
-- `keepass_key` - [*optional*] path to keyfile
+- `keepass_psw` - password
+- `keepass_key` - *Optional*. Path to keyfile
+- `keepass_ttl` - *Optional*. Socket TTL (will be closed automatically when not used). 
+Default 60 seconds.
 
 
-## Usage with UNIX socket
+## Usage
 
-> _This usage is more preferred for performance reason, 
-because of KeePass file stay decrypted and not need to reopen after done each playbook task 
-[(see the issue for more info)](https://github.com/viczem/ansible-keepass/issues/1)_
+`ansible-doc -t lookup keepass` to get description of the plugin
 
-In this case, there is no need to enter a password for KeePass each time Ansible is called.
-Run socket by the command and after that enter a password to make to open KeePass file.
+> **WARNING**: For security reasons, do not store KeePass passwords in plain text. 
+Use `ansible-vault encrypt_string` to encrypt it and use it like below
 
-**Supported only Python 3**
+    # file: group_vars/all
 
-    python3 kpsock.py ~/.keepass/database.kdbx
+    keepass_dbx: "~/.keepass/database.kdbx"
+    keepass_psw: !vault |
+          $ANSIBLE_VAULT;1.1;AES256
+          ...encrypted password...
 
-
-The command will creates UNIX socket in a system temp directory. Only one socket 
-> **WARNING**: The KeePass file and password are stay decrypted in memory while the socket is open.
-
-The socket timeout is 1 minute since past access (will be closed automatically when not used).
-To change timeout use `--ttl` argument. 
-For logging requests in a file use `--log` (default `--log-level` is `INFO`).
-
-For help `python kpsock.py --help`
-
-To send the running command in background press <kbd>CTRL</kbd>+<kbd>Z</kbd> and execute `bg` 
-(`fg` to get the job into the foreground again).
-
-
-## Example
-
-Define variables you need e.g. in any file in group_vars
-
+### Example
 
     ansible_user             : "{{ lookup('keepass', 'path/to/entry', 'username') }}"
     ansible_become_pass      : "{{ lookup('keepass', 'path/to/entry', 'password') }}"
-    ansible_custom_field     : "{{ lookup('keepass', 'path/to/entry', 'custom_field_property', true) }}"
-    ansible_all_custom_fields: "{{ lookup('keepass', 'path/to/entry', '*', true) }}"
+    ansible_custom_field     : "{{ lookup('keepass', 'path/to/entry', 'custom_properties', 'a_custom_property_name') }}"
 
-
-You can get another [properties of an KeePass entry](https://github.com/pschmitt/pykeepass/blob/master/pykeepass/entry.py)
-(not only `username` or `password`)
-
-Specify a boolean value of true to use custom field properties
- 
-`ansible-doc -t lookup keepass` - to get description of the plugin
+More examples see in [/examples](/examples).
--- keepass.py
+++ keepass.py
@@ -1,27 +1,30 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-try:
-    from __main__ import display
-except ImportError:
-    from ansible.utils.display import Display
-    display = Display()
-
 import os
-import json
+import re
 import socket
 import tempfile
+from pykeepass import PyKeePass
+from pykeepass.exceptions import CredentialsError
 from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
+import time
+import subprocess
+import sys
+import argparse
+import getpass
+import hashlib
+import traceback
+import stat
 
 
 DOCUMENTATION = """
     lookup: keepass
-    author: Victor Zemtsov <victor.zemtsov@gmail.com>
-    version_added: '0.4.0'
-    short_description: fetch data from KeePass file
+    author: Victor Zemtsov <viczem.dev@gmail.com>
+    version_added: '0.4.1'
+    short_description: Fetching data from KeePass file
     description:
         - This lookup returns a value of a property of a KeePass entry 
         - which fetched by given path
@@ -30,62 +33,332 @@ DOCUMENTATION = """
         description: 
           - first is a path to KeePass entry
           - second is a property name of the entry, e.g. username or password
-          - third (optional property) if true custem_field_property is return
         required: True
     notes:
       - https://github.com/viczem/ansible-keepass
     
-    example:
+    examples:
+      - "{{ lookup('keepass', 'path/to/entry', 'username') }}"
       - "{{ lookup('keepass', 'path/to/entry', 'password') }}"
+      - "{{ lookup('keepass', 'path/to/entry', 'custom_properties', 'a_custom_property_name') }}"
 """
+
+
+display = Display()
 
 
 class LookupModule(LookupBase):
     keepass = None
 
+    def _var(self, var_value):
+        return self._templar.template(var_value, fail_on_undefined=True)
+
     def run(self, terms, variables=None, **kwargs):
-        if not terms or len(terms) < 2 or len(terms) > 3:
-            raise AnsibleError('Wrong request format')
+        if not terms:
+            raise AnsibleError("KeePass: arguments is not set")
+        if not all(isinstance(_, str) for _ in terms):
+            raise AnsibleError("KeePass: invalid argument type, all must be string")
 
         if variables is not None:
             self._templar.available_variables = variables
-        variables_for_templating = getattr(self._templar, '_available_variables', {})
+        variables_ = getattr(self._templar, "_available_variables", {})
 
-        entry_path = terms[0].strip('/')
-        entry_attr = terms[1]
-        enable_custom_attr = False
-        
-        if len(terms) == 3:
-            enable_custom_attr = terms[2]
-        
-        kp_dbx = self._templar.template(variables_for_templating.get('keepass_dbx', ''), fail_on_undefined=True)
-        kp_dbx = os.path.realpath(os.path.expanduser(kp_dbx))
-        if os.path.isfile(kp_dbx):
-            display.v(u"Keepass: database file %s" % kp_dbx)
+        # Check keepass database file (required)
+        var_dbx = self._var(variables_.get("keepass_dbx", ""))
+        if not var_dbx:
+            raise AnsibleError("KeePass: 'keepass_dbx' is not set")
+        var_dbx = os.path.realpath(os.path.expanduser(os.path.expandvars(var_dbx)))
+        if not os.path.isfile(var_dbx):
+            raise AnsibleError("KeePass: '%s' is not found" % var_dbx)
 
-        kp_soc = "%s/ansible-keepass.sock" % tempfile.gettempdir()
-        if os.path.exists(kp_soc):
-            display.v(u"Keepass: fetch from socket")
-            return self._fetch_socket(kp_soc, entry_path, entry_attr, enable_custom_attr)
+        # Check key file (optional)
+        var_key = self._var(variables_.get("keepass_key", ""))
+        if var_key:
+            var_key = os.path.realpath(os.path.expanduser(os.path.expandvars(var_key)))
+            if not os.path.isfile(var_key):
+                raise AnsibleError("KeePass: '%s' is not found" % var_key)
 
-    def _fetch_socket(self, kp_soc, entry_path, entry_attr, enable_custom_attr):
-        display.vvvv(u"KeePass: try to socket connect")
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.connect(kp_soc)
-        display.vvvv(u"KeePass: connected")
-        data = {'attr': entry_attr, 'path': entry_path}
-        if enable_custom_attr:
-          data['enable_custom_attr'] = True
-        sock.send(json.dumps(data).encode())
-        display.vv(u"KeePass: attr: %s in path: %s" % (entry_attr, entry_path))
+        # Check password (required)
+        var_psw = self._var(variables_.get("keepass_psw", ""))
+        if not var_psw:
+            raise AnsibleError("KeePass: 'keepass_psw' is not set")
+
+        # TTL of keepass socket (optional, default: 60 seconds)
+        var_ttl = self._var(str(variables_.get("keepass_ttl", 60)))
+
+        # UNIX socket path for a dbx (supported multiple dbx)
+        tempdir = tempfile.gettempdir()
+        if not os.access(tempdir, os.W_OK):
+            raise AnsibleError("KeePass: no write permissions to '%s'" % tempdir)
+
+        socket_path_suffix = hashlib.sha1(
+            ("%s%s" % (getpass.getuser(), var_dbx)).encode()
+        ).hexdigest()[:8]
+        socket_path = "%s/ansible-keepass-%s.sock" % (tempdir, socket_path_suffix)
+
         try:
-            msg = json.loads(sock.recv(1024).decode())
-        except json.JSONDecodeError as e:
+            # If UNIX socket file is not exists then the socket is not running
+            stat.S_ISSOCK(os.stat(socket_path).st_mode)
+        except FileNotFoundError:
+            cmd = [
+                "/usr/bin/env",
+                "python3",
+                os.path.abspath(__file__),
+                var_dbx,
+                socket_path,
+                var_ttl,
+            ]
+            if var_key:
+                cmd.append("--key=%s" % var_key)
+            try:
+                display.v("KeePass: run socket for %s" % var_dbx)
+                subprocess.Popen(cmd)
+            except OSError:
+                raise AnsibleError(traceback.format_exc())
+
+            attempts = 10
+            success = False
+            for _ in range(attempts):
+                try:
+                    display.vvv("KeePass: try connect to socket %s/%s" % (_, attempts))
+                    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                    sock.connect(socket_path)
+                    # send password to the socket for decrypt keepass dbx
+                    display.vvv("KeePass: send password to '%s'" % socket_path)
+                    sock.send(_rq("password", str(var_psw)))
+                    resp = sock.recv(1024).decode().splitlines()
+
+                    if len(resp) == 2 and resp[0] == "password":
+                        if resp[1] == "0":
+                            success = True
+                        else:
+                            sock.send(_rq("close"))
+                            raise AnsibleError("KeePass: wrong dbx password")
+                    sock.close()
+                    break
+                except FileNotFoundError:
+                    # wait until the above command open the socket
+                    time.sleep(1)
+
+            if not success:
+                raise AnsibleError("KeePass: socket connection failed for %s" % var_dbx)
+            display.v("KeePass: open socket for %s -> %s" % (var_dbx, socket_path))
+
+        if len(terms) == 1 and terms[0] in ("quit", "exit", "close"):
+            self._send(socket_path, terms[0], [])
+        else:
+            # Fetching data from the keepass socket
+            return self._send(socket_path, "fetch", terms)
+
+    def _send(self, kp_soc, cmd, terms):
+        display.vvv("KeePass: connect to '%s'" % kp_soc)
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+        try:
+            sock.connect(kp_soc)
+        except FileNotFoundError:
+            raise AnsibleError("KeePass: '%s' is not found" % kp_soc)
+
+        try:
+            display.vvv("KeePass: %s %s" % (cmd, terms))
+            sock.send(_rq(cmd, *terms))
+
+            resp = sock.recv(1024).decode().splitlines()
+            resp_len = len(resp)
+            if resp_len == 0:
+                raise AnsibleError("KeePass: '%s' result is empty" % cmd)
+
+            if resp_len == 3:
+                if resp[0] != cmd:
+                    raise AnsibleError(
+                        "KeePass: received command '%s', expected '%s'" % (resp[0], cmd)
+                    )
+                if resp[1] == "0":
+                    return [resp[2]]
+                else:
+                    raise AnsibleError("KeePass: '%s' has error '%s'" % (resp[2], cmd))
+
+        except Exception as e:
             raise AnsibleError(str(e))
         finally:
             sock.close()
-            display.vvvv(u"KeePass: disconnected")
+            display.vvv("KeePass: disconnect from '%s'" % kp_soc)
 
-        if msg['status'] == 'error':
-            raise AnsibleError(msg['text'])
-        return [msg['text']]
+
+def _keepass_socket(kdbx, kdbx_key, sock_path, ttl=60):
+    """
+
+    :param str kdbx:
+    :param str kdbx_key:
+    :param str sock_path:
+    :param int ttl: in seconds
+    :return:
+
+    Socket messages have multiline format.
+    First line is a command for both messages are request and response
+    """
+    try:
+        os.umask(0o177)
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.bind(sock_path)
+            s.listen(1)
+            s.settimeout(ttl)
+            kp = None
+            is_open = True
+
+            while is_open:
+                conn, addr = s.accept()
+                with conn:
+                    conn.settimeout(ttl)
+                    while True:
+                        data = conn.recv(1024).decode()
+                        if not data:
+                            break
+
+                        rq = data.splitlines()
+                        if len(rq) == 0:
+                            conn.send(_resp("", 1, "empty request"))
+                            break
+
+                        cmd, *arg = rq
+                        arg_len = len(arg)
+
+                        # CMD: quit | exit | close
+                        if arg_len == 0 and cmd in ("quit", "exit", "close"):
+                            conn.send(_resp(cmd, 0))
+                            conn.close()
+                            is_open = False
+                            break
+
+                        # CMD: password
+                        if kp is None:
+                            if arg_len == 0:
+                                conn.send(_resp("password", 1))
+                                break
+                            if cmd == "password" and arg[0]:
+                                kp = PyKeePass(kdbx, arg[0], kdbx_key)
+                                conn.send(_resp("password", 0))
+                                break
+                            else:
+                                conn.send(_resp("password", 1))
+                                break
+
+                        # CMD: fetch
+                        # Read data from decrypted KeePass file
+                        if cmd != "fetch":
+                            conn.send(_resp("fetch", 1, "unknown command '%s'" % cmd))
+                            break
+
+                        if arg_len == 0:
+                            conn.send(_resp("fetch", 1, "path is not set"))
+                            break
+
+                        if arg_len == 1:
+                            conn.send(
+                                _resp(
+                                    "fetch",
+                                    1,
+                                    "property name is not set for '%s'" % arg[0],
+                                )
+                            )
+                            break
+
+                        path = [
+                            _.replace("\\/", "/")
+                            for _ in re.split(r"(?<!\\)/", arg[0])
+                            if _ != ""
+                        ]
+                        entry = kp.find_entries_by_path(path, first=True)
+
+                        if entry is None:
+                            conn.send(
+                                _resp("fetch", 1, "path '%s' is not found".format(path))
+                            )
+                            break
+
+                        prop = arg[1]
+                        if prop == "custom_properties":
+                            if arg_len == 2:
+                                conn.send(
+                                    _resp(
+                                        "fetch",
+                                        1,
+                                        "custom_property key is not set "
+                                        "for '%s'".format(arg[0]),
+                                    )
+                                )
+                                break
+
+                            prop_key = arg[2]
+                            if prop_key not in entry.custom_properties:
+                                conn.send(
+                                    _resp(
+                                        "fetch",
+                                        1,
+                                        "custom_property '%s' is not found "
+                                        "for '%s'".format(prop_key, path),
+                                    )
+                                )
+                                break
+                            conn.send(
+                                _resp(
+                                    "fetch",
+                                    0,
+                                    entry.get_custom_property(prop_key),
+                                )
+                            )
+                            break
+
+                        if not hasattr(entry, prop):
+                            conn.send(
+                                _resp(
+                                    "fetch",
+                                    1,
+                                    "unknown property '%s' for '%s'".format(prop, path),
+                                )
+                            )
+                            break
+                        conn.send(_resp("fetch", 0, getattr(entry, prop)))
+    except CredentialsError:
+        print("%s failed to decrypt" % kdbx)
+        sys.exit(1)
+    except FileNotFoundError as e:
+        print(str(e))
+        sys.exit(1)
+    except ValueError as e:
+        print(str(e))
+        sys.exit(1)
+    except socket.timeout:
+        pass
+    finally:
+        if os.path.exists(sock_path):
+            os.remove(sock_path)
+
+
+def _rq(cmd, *arg):
+    """Request to keepass socket
+
+    :param str cmd: Command name
+    :param arg: Arguments
+    """
+    return "\n".join((cmd, *arg)).encode()
+
+
+def _resp(cmd, status_code, payload=""):
+    """Response from keepass socket
+
+    :param str cmd: Command name
+    :param int status_code: == 0 - no error; 1 - an error
+    :param payload: A data from keepass or error description
+    """
+    return "\n".join((cmd, str(status_code), str(payload))).encode()
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("kdbx", type=str)
+    arg_parser.add_argument("kdbx_sock", type=str)
+    arg_parser.add_argument("ttl", type=int, default=60)
+    arg_parser.add_argument("--key", type=str, nargs="?", default=None)
+    args = arg_parser.parse_args()
+    _keepass_socket(args.kdbx, args.key, args.kdbx_sock, args.ttl)
